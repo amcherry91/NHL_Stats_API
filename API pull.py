61eb8e2a22589670dcfe2e00bca3a37963b79fd9
Alex Cherry <55280038+amcherry91@users.noreply.github.com>
2019-10-22 19:17:30 +0200

    Script to pull data from the api and the produced CSV file

---
+++ API pull.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import csv
+import pandas as pd
+
+# go to https://gitlab.com/dword4/nhlapi/blob/master/stats-api.md#teams for documentation and list of directories
+
+response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
+print(response.json())
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
+
+r = response.json()
+data = (r["teams"])
+c = csv.writer(open("team_data_api.csv", "w"), lineterminator= "\n")
+
+for item in r["teams"]:
+    c.writerow([item["id"], item["name"], item["abbreviation"], item["teamName"], item["link"], item["venue"], item["locationName"],
+               item["firstYearOfPlay"], item["division"], item["conference"], item["franchise"], item["shortName"]])
+
+
+
+
+
+
+
+
