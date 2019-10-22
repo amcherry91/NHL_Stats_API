3e2d331b8d97cea010c82c68977215030e69e5e3
Alex Cherry <alex.cherry47@gmail.com>
2019-10-22 21:51:23 +0200

    Began work on parsing current standings into a csv document

--- API pull.py
+++ API pull.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import urllib.request
+from urllib.request import urlopen
 import csv
 import pandas as pd
 
 # go to https://gitlab.com/dword4/nhlapi/blob/master/stats-api.md#teams for documentation and list of directories
 
-response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
+"""response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
 print(response.json())
 
 def jprint(obj):
@@ -14,20 +16,26 @@ def jprint(obj):
 
 jprint(response.json())
 
+# get current team information
 r = response.json()
-data = (r["teams"])
+data_teams = (r["teams"])
 c = csv.writer(open("team_data_api.csv", "w"), lineterminator= "\n")
 
 for item in r["teams"]:
     c.writerow([item["id"], item["name"], item["abbreviation"], item["teamName"], item["link"], item["venue"], item["locationName"],
-               item["firstYearOfPlay"], item["division"], item["conference"], item["franchise"], item["shortName"]])
+               item["firstYearOfPlay"], item["division"], item["conference"], item["franchise"], item["shortName"]])"""
 
+# get current team standings
+response_standings = requests.get("https://statsapi.web.nhl.com/api/v1/standings")
+print(response_standings.json())
+c = csv.writer(open("team_standings.csv", "w"), lineterminator= "\n")
 
+with urlopen("https://statsapi.web.nhl.com/api/v1/standings") as response:
+    source_standings = response.read()
 
+data_standings = json.loads(source_standings)
+# print(json.dumps(data_standings, indent= 2))
 
-
-
-
-
-
+for item in data_standings["records"]:
+    c.writerow([item["teamRecords"]])
 
