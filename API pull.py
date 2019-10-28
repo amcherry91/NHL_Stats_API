68c8702fbd51f5fbb1fd18294689eb07d42cd43f
Alex Cherry <alex.cherry47@gmail.com>
2019-10-28 20:59:35 +0100

    Updated to get stats for all players on a specific team. Code still under construction

--- API pull.py
+++ API pull.py
@@ -2,31 +2,53 @@ import requests
 import json
 import csv
 import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 # go to https://gitlab.com/dword4/nhlapi/blob/master/stats-api.md#teams for documentation and list of directories
+
 url = 'https://statsapi.web.nhl.com/api/v1/teams'
-team_endpoint = 'https://statsapi.web.nhl.com/api/v1/teams/12'
-roster_endpoint = 'https://statsapi.web.nhl.com/api/v1/teams/12/roster'
-r = requests.get(url + team_endpoint + '/' + str(id) + roster_endpoint)
+r = requests.get(url)
 teams = pd.io.json.json_normalize(r.json()['teams'])
-team_id = teams[teams['teamName'] == 'Hurricanes']['id'].values[0]
-roster = pd.io.json.json_normalize(r.json()['roster'])
-print(roster)
+#print(teams[teams['name']== 'Nashville Predators']['id'])
+
+url_1 = 'https://statsapi.web.nhl.com/api/v1/teams/18/roster'
+r_1 = requests.get(url_1)
+roster = r_1.json()['roster']
+roster = pd.io.json.json_normalize(r_1.json()['roster'])
 
 df = pd.DataFrame()
-for p_link in roster['person.link']:
-    r = requests.get(url + p_link + '/stats', params={'stats':'gameLog', 'season':'20182019'})
-    stats = pd.io.json.json_normalize(r.json()['stats'][0]['splits'])
+for pid in roster['person.id']:
+    url_2 = 'https://statsapi.web.nhl.com/api/v1/people/' + str(pid) + '/stats'
+    params = {'stats': 'gameLog', 'season': '20182019'}
+    r_2 = requests.get(url_2, params=params)
+    stats = pd.io.json.json_normalize(r_2.json()['stats'][0]['splits'])
     if not stats.empty:
         stats = stats.groupby(['team.name']).aggregate(sum)
-        stats['playerName'] = roster[roster['person.link'] == p_link]['person.fullName'].values[0]
+        stats['Name'] = roster[roster['person.id'] == pid]['person.fullName'].values[0]
         df = pd.concat([df, stats], ignore_index=True, sort=False)
     else:
-        print('No stats for ' + roster[roster['person.link'] == p_link]['person.fullName'].values[0])
+        print('No stats for ' + roster[roster['person.id'] == pid]['person.fullName']).values[0]
+print(df.columns)
 
-keep_cols = ['stat.assists', 'stat.games', 'stat.goals', 'stat.hits', 'stat.points', 'playerName']
+keep_cols = ['team.id', 'Name', 'stat.games', 'stat.points', 'stat.goals', 'stat.assists', 'stat.plusMinus', 'stat.hits']
 df = df[keep_cols]
-rename_cols = {'stat.assist': 'assists', 'stat.games': 'games', 'stat.goals': 'goals', 'stat.hits': 'hits',
-               'stat.points': 'points'}
+rename_cols = {
+    'stat.assists': 'Assists',
+    'stat.games': 'Games',
+    'stat.goals': 'Goals',
+    'stat.hits': 'Hits',
+    'stat.points': 'Points',
+    'team.id': 'id',
+    'stat.plusMinus': 'PlusMinus',
+    'Name': 'PlayerName'
+}
+
+df.rename(columns=rename_cols, inplace=True)
+df.head()
+print(df)
+
+df.to_csv('player_stats_20182019.csv', index=False)
 
 # code is WIP
